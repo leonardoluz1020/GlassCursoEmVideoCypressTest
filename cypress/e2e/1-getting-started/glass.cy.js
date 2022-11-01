7cb077c28784404d5dfc4a485bb07684b9cc3aa8
Leonardo Luz <leonardoluz10@hotmail.com>
2022-11-01 19:19:31 -0300

    Analisando titulo e verificando links

--- cypress/e2e/1-getting-started/glass.cy.js
+++ cypress/e2e/1-getting-started/glass.cy.js
@@ -2,7 +2,15 @@ describe('Teste de interface na pagina inicial do google glass',()=>{
     beforeEach(()=>{
         cy.visit('../../../src/index.html')
     })
-    it('Testes',()=>{
+    it('Verificando Titulo da Pagina',()=>{
+        cy.title().should('be.equal','Tudo sobre Google Glass');
+    })
+    it('Verificando links da pagina ',()=>{
+        cy.contains('a','Home').should('be.visible').click()
+        cy.contains('a','Especificações').should('be.visible').click()
+        cy.contains('a','Fotos').should('be.visible').click()
+        cy.contains('a','Multimídia').should('be.visible').click()
+        cy.contains('a','Fale conosco').should('be.visible').click()
 
     })
 })
